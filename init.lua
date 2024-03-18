dda0d6838a9fe39afec201f8e98b42b265375cb2
Hugo Vaz <hugomartinsvaz@gmail.com>
2024-03-18 08:34:33 -0300

    instalando gramática de blade no tree-sitter

--- init.lua
+++ init.lua
@@ -278,6 +278,7 @@ require('lazy').setup {
         lua = { 'stylua' },
         python = { 'isort', 'black' },
         javascript = { { 'prettierd', 'prettier' } },
+        blade = { 'blade-formatter' },
       },
     },
   },
@@ -387,10 +388,19 @@ require('lazy').setup {
     build = ':TSUpdate',
     config = function()
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = 'https://github.com/EmranMR/tree-sitter-blade',
+          files = { 'src/parser.c' },
+          branch = 'main',
+        },
+        filetype = 'blade',
+      }
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'php', 'blade' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
